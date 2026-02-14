84e06197729871171cde78cb2f96a14cc29d5ec0
Sam Al Jaboury <saljaboury1@st.noorderpoort.nl>
2026-02-14 23:00:49 +0100

    check for route

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 require_once "./Router/routerController.php";
 require_once "./Router/config.php";
 
+//inject dependancy into class
+$test = new Router($conn);
 //check if route exists
 if ($test->handleRequest()) {
+    
     var_dump($test->handleRequest()->showAll());
 }
 
