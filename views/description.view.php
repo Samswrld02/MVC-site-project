e7710a8129ae76fb1517e1f428c86896a06d0d44
Sam Al Jaboury <saljaboury1@st.noorderpoort.nl>
2026-02-23 12:20:35 +0100

    fixed edit button -> editfunctionality still working on

--- views/description.view.php
+++ views/description.view.php
@@ -1,7 +1,7 @@
 
 <h1><?=  $descriptionArray[0]["title"] ?></h1>
 <a class = "buttonReturn" href="<?= URLROOT?>/home">Terug</a>
-<a class = "editButton" href="<?= URLROOT ?>/edit/show/<?= array_key_exists("length_in_minutes", $descriptionArray[0]) ? "movies" : "series"?>/<?=$descriptionArray[0]['id']?>">Edit</a>
+<a class = "editButton" href="<?= URLROOT ?>/edit/show/<?= $descriptionArray[0]['seasons'] == null ? "movie" : "series"?>/<?=$descriptionArray[0]['id']?>">Edit</a>
 
 <?php require_once "./views/components/descriptionComponents/description.table.php" ?>
     <!-- <?=  var_dump($descriptionArray) ?> -->
